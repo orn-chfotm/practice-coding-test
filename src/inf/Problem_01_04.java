b7eaf821bbc55abaaf1eff5d9b3f4ac10cff9298
orn-chfotm <chfotm@gmail.com>
2025-05-07 15:48:59 +0900

    2025-05-07 인프런 자바(Java) 알고리즘 문제풀이 : 01-04

---
+++ src/inf/Problem_01_04.java
@@ -0,0 +1,33 @@
+package inf;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem_01_04 {
+
+    public static void solution(List<String> words) {
+        for (String word : words) {
+            for (int i = word.length() - 1; i >= 0; i--) {
+                System.out.print(word.charAt(i));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
+
+            List<String> words = new ArrayList<>();
+
+            for (int i = 0; i < N; i++) {
+                words.add(br.readLine());
+            }
+
+            solution(words);
+        }
+    }
+}
