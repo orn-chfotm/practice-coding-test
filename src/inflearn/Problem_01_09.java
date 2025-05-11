209a5f7c5ec88980df532a3695df801c5b9ffc38
orn-chfotm <chfotm@gmail.com>
2025-05-11 22:02:21 +0900

    2025-05-11 인프런 Java 알고리즘 문제풀이 : 01-09 - 숫자만 추출

---
+++ src/inflearn/Problem_01_09.java
@@ -0,0 +1,24 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem_01_09 {
+
+    public static int solution(String str) {
+        String result = "";
+        char[] charArr = str.toCharArray();
+        for (char c : charArr) {
+            if (!Character.isAlphabetic(c)) {
+                result += c;
+            }
+        }
+
+        return Integer.parseInt(result);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+    }
+}
