bdc35d1904ae30cc76bd107d315e4a5dd1644f1f
orn-chfotm <chfotm@gmail.com>
2025-05-11 21:52:22 +0900

    2025-05-11 인프런 Java 알고리즘 문제풀이 : 01-08 - 유효한 팰린드롬

---
+++ src/inflearn/Problem_01_08.java
@@ -0,0 +1,34 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem_01_08 {
+
+    public static boolean solution(String str) {
+        int lt = 0, rt = str.length() - 1;
+
+        while (lt < rt) {
+            if (!Character.isAlphabetic(str.charAt(lt))) {
+                lt++;
+            } else if (!Character.isAlphabetic(str.charAt(rt))) {
+                rt--;
+            } else {
+                String strLeft = String.valueOf(str.charAt(lt));
+                String strRight = String.valueOf(str.charAt(rt));
+                if (!strLeft.equalsIgnoreCase(strRight)) {
+                    return false;
+                }
+                lt++;
+                rt--;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(solution(str) ? "YES" : "NO");
+    }
+}
