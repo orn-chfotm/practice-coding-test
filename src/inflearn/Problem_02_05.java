8715e52b4e91a91336ed8439731308f3367ab2f6
orn-chfotm <chfotm@gmail.com>
2025-05-22 00:28:06 +0900

    2025-05-22 인프런 Java 알고리즘 문제풀이 : 02-05

---
+++ src/inflearn/Problem_02_05.java
@@ -0,0 +1,26 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem_02_05 {
+    public static int solution(int n) {
+        int count = 0;
+        int[] check = new int[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            if (check[i] == 0) {
+                count++;
+                for (int j = i; j <= n; j=j+i) {
+                    check[j] = 1;
+                }
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(solution(n));
+    }
+}
