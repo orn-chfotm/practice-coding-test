ce471c1cb30426bd2030bdd8149fa1c82fd3d82a
orn-chfotm <chfotm@gmail.com>
2025-05-27 00:00:46 +0900

    2025-05-22 인프런 Java 알고리즘 문제풀이 : 02-07

---
+++ src/inflearn/Problem_02_07.java
@@ -0,0 +1,35 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem_02_07 {
+
+    public int solution(int n, int[] arr) {
+        int answer = 0;
+
+        int score = 1;
+        for (int i : arr) {
+            if (i == 1) {
+                answer += score;
+                score += 1;
+            } else {
+                score = 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Problem_02_07 problem = new Problem_02_07();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print(problem.solution(n, arr));
+    }
+}
