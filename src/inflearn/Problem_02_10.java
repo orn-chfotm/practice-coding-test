fad3d517678be9a531531d88f5c2c7b822989c22
orn-chfotm <chfotm@gmail.com>
2025-06-03 01:06:08 +0900

    2025-06-02 인프런 Java 알고리즘 문제풀이 : 02-10

---
+++ src/inflearn/Problem_02_10.java
@@ -0,0 +1,50 @@
+package inflearn;
+
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Problem_02_10 {
+
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.print(solution(n, arr));
+    }
+
+    private static int solution(int n, int[][] arr) {
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+
+                    if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                        if (arr[nx][ny] >= arr[i][j]) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+                if (flag) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
