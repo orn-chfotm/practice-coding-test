639dbbe825d1c6d472d324d87f2947df29b938ad
orn-chfotm <chfotm@gmail.com>
2025-07-14 04:12:54 +0900

    2025-07-14 인프런 Java 알고리즘 문제풀이 : 08-05

---
+++ src/inflearn/problem_08/Problem_08_05.java
@@ -0,0 +1,47 @@
+package inflearn.problem_08;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem_08_05 {
+
+    int n, m;
+    int answer = Integer.MAX_VALUE;
+
+    public int solution(int n, int m, int[] arr) {
+        this.n = n;
+        this.m = m;
+
+        dfs(n - 1, 0, m, arr);
+
+        return answer;
+    }
+
+    public void dfs(int index, int count, int change, int[] arr) {
+        if (change < 0) return;
+        if (count >= answer) return;
+
+        if (index < 0) {
+            if (change == 0) {
+                answer = Math.min(answer, count);
+            }
+            return;
+        }
+
+        dfs(index, count + 1, change - arr[index], arr);
+        dfs(index - 1, count, change, arr);
+    }
+
+    public static void main(String[] args) {
+        Problem_08_05 main = new Problem_08_05();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int m = sc.nextInt();
+
+        System.out.println(main.solution(n, m, arr));
+    }
+}
